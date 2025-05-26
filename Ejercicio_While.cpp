f7e678ca9b7bed7bdf8646a46b94ba907c9639ff
Alvarez-Morales13 <00034725@uca.edu.sv>
2025-05-26 17:52:43 -0600

    ejercicios

--- Ejercicio_While.cpp
+++ Ejercicio_While.cpp
@@ -4,18 +4,24 @@ using namespace std;
 int main(){
     //Declaración de las variables
     int numero1;
+    int suma = 0;
 
     cout << "Ingrese un numero entero: " << endl;
+    cin >> numero1;
 
     while (numero1 >= 0)
-    {   cin >> numero1;
-        for (int i = 0; i > 0; i++)
-    {  
-        
+    {
+        for (int i = 0; i < 1; i++)
+    {   
+        suma += numero1;
     }
 
+    cout << "Ingrese un numero entero: " << endl;
+    cin >> numero1;
     
     }
+
+    cout << "La suma de los numeros positivos es: " << suma << endl;
 
    return 0; 
 }
